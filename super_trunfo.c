21bd56b8eba38fe1357f2288b54ea65f9ad73fd3
Osvaldo Alves <oalves.neto@outlook.com>
2025-04-17 10:30:00 -0300

    feat: add structure to card02 fix: improving code and fix \n on buffer.

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,3 +1,4 @@
+#include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,50 +13,85 @@ struct Card {
 };
 
 int main() {
+    SetConsoleOutputCP(CP_UTF8);  // set UTF-8 format
     struct Card card01, card02;
     int timer = 1000;
 
     // Start program
-    printf("Seja bem vindo ao Super trunfo!\n");
+    printf("\nSeja bem vindo ao Super Trunfo!\n");
     _sleep(timer);
 
-    printf("Cadastre a primeira carta (1 de 2):\n");
+    printf("Cadastre a 1º carta (1 de 2):\n");
     _sleep(timer);
 
     // Scan Card01 data
     printf("Qual o estado?:\n");
-    scanf("%c", &card01.state);
+    scanf(" %c", &card01.state);
 
-    printf("Digite o codigo da carta:\n");
+    printf("Digite o código da carta:\n");
     scanf("%s", &card01.code);
 
     printf("Qual o nome da cidade?:\n");
     scanf("%s", &card01.city);
 
-    printf("Qual a populacao da cidade?:\n");
+    printf("Qual a população da cidade?:\n");
     scanf("%i", &card01.population);
 
-    printf("Qual a area da cidade (em Km²)?:\n");
+    printf("Qual a área da cidade (em Km²)?:\n");
     scanf("%f", &card01.area);
 
     printf("Qual o PIB da cidade?:\n");
     scanf("%f", &card01.pib);
 
-    printf("Qual a quantidade total de pontos turisticos da cidade?:\n");
+    printf("Qual a quantidade total de pontos turísticos da cidade?:\n");
     scanf("%i", &card01.total_turism_points);
 
+    printf("\nCadastre a 2° carta (2 de 2):\n");
+    _sleep(timer);
+
+    // Scan Card02 data
+    printf("Qual o estado?:\n");
+    scanf(" %c", &card02.state);
+
+    printf("Digite o código da carta:\n");
+    scanf("%s", &card02.code);
+
+    printf("Qual o nome da cidade?:\n");
+    scanf("%s", &card02.city);
+
+    printf("Qual a população da cidade?:\n");
+    scanf("%i", &card02.population);
+
+    printf("Qual a área da cidade (em Km²)?:\n");
+    scanf("%f", &card02.area);
+
+    printf("Qual o PIB da cidade?:\n");
+    scanf("%f", &card02.pib);
+
+    printf("Qual a quantidade total de pontos turísticos da cidade?:\n");
+    scanf("%i", &card02.total_turism_points);
 
     // Print data
-    printf("Processando informacoes...\n\n");
     _sleep(timer);
+    printf("Processando informacoes...\n\n");
 
     // Print Card01 data
     printf("###### Carta 01 ######\n");
     printf("Estado: %c\n", card01.state);
-    printf("Estado: %s\n", card01.code);
+    printf("Código: %s\n", card01.code);
     printf("Cidade: %s\n", card01.city);
-    printf("Populacao: %i\n", card01.population);
-    printf("Area: %f Km²\n", card01.area);
-    printf("PIB: R$ %f A\n", card01.pib);
-    printf("Numero de Pontos Turisticos: %i\n", card01.total_turism_points);
+    printf("População: %i\n", card01.population);
+    printf("Área: %f Km²\n", card01.area);
+    printf("PIB: R$ %f bi\n", card01.pib);
+    printf("Número de Pontos Turísticos: %i\n", card01.total_turism_points);
+
+    // Print Card01 data
+    printf("\n###### Carta 02 ######\n");
+    printf("Estado: %c\n", card02.state);
+    printf("Código: %s\n", card02.code);
+    printf("Cidade: %s\n", card02.city);
+    printf("População: %i\n", card02.population);
+    printf("Área: %f Km²\n", card02.area);
+    printf("PIB: R$ %f bi\n", card02.pib);
+    printf("Número de Pontos Turísticos: %i\n", card02.total_turism_points);
 }
