63a00efc232689dd4090d4429c68496888846cfe
Osvaldo Alves <oalves.neto@outlook.com>
2025-05-01 15:28:00 -0300

    feat: update super trunfo with adventure level challenge requirementes.

--- super_trunfo.c
+++ super_trunfo.c
@@ -14,84 +14,106 @@ struct Card {
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);    // set UTF-8 format
-    struct Card card01, card02;
+    struct Card card01 = {'S', "001", "Cidade A", 500000, 1500.0, 120.5, 10};
+    struct Card card02 = {'N', "002", "Cidade B", 450000, 1000.0, 140.0, 12};
+
     int timer = 1000;
+    int option;
+    float density1, density2;
 
-    // Start program
-    printf("\nSeja bem vindo ao Super Trunfo!\n");
+    density1 = (float)card01.population / card01.area;
+    density2 = (float)card02.population / card02.area;
+
+    printf("\nBem-vindo ao Super Trunfo!\n\n");
+
+    printf("Escolha o atributo para comparar:\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("3 - PIB\n");
+    printf("4 - Número de Pontos Turísticos\n");
+    printf("5 - Densidade Demográfica\n");
+
+    // Get data
+    printf("Digite a opção escolhida: ");
+    scanf("%d", &option);
+
+    printf("\nProcessando comparação...\n\n");
     _sleep(timer);
 
-    printf("Cadastre a 1º carta (1 de 2):\n");
-    _sleep(timer);
+    switch (option) {
+        case 1:
+            printf("Comparando População:\n");
+            printf("%s: %d habitantes\n", card01.city, card01.population);
+            printf("%s: %d habitantes\n", card02.city, card02.population);
 
-    // Scan Card01 data
-    printf("Qual o estado?:\n");
-    scanf(" %c", &card01.state);
+            if (card01.population > card02.population) {
+                printf("\n%s venceu!\n", card01.city);
+            } else if (card02.population > card01.population) {
+                printf("\n%s venceu!\n", card02.city);
+            } else {
+                printf("\nEmpate!\n");
+            }
+            break;
 
-    printf("Digite o código da carta:\n");
-    scanf("%s", &card01.code);
+        case 2:
+            printf("Comparando Área:\n");
+            printf("%s: %.2f Km²\n", card01.city, card01.area);
+            printf("%s: %.2f Km²\n", card02.city, card02.area);
 
-    printf("Qual o nome da cidade?:\n");
-    scanf("%s", &card01.city);
+            if (card01.area > card02.area) {
+                printf("\n%s venceu!\n", card01.city);
+            } else if (card02.area > card01.area) {
+                printf("\n%s venceu!\n", card02.city);
+            } else {
+                printf("\nEmpate!\n");
+            }
+            break;
 
-    printf("Qual a população da cidade?:\n");
-    scanf("%i", &card01.population);
+        case 3:
+            printf("Comparando PIB:\n");
+            printf("%s: R$ %.2f bi\n", card01.city, card01.pib);
+            printf("%s: R$ %.2f bi\n", card02.city, card02.pib);
 
-    printf("Qual a área da cidade (em Km²)?:\n");
-    scanf("%f", &card01.area);
+            if (card01.pib > card02.pib) {
+                printf("\n%s venceu!\n", card01.city);
+            } else if (card02.pib > card01.pib) {
+                printf("\n%s venceu!\n", card02.city);
+            } else {
+                printf("\nEmpate!\n");
+            }
+            break;
 
-    printf("Qual o PIB da cidade?:\n");
-    scanf("%f", &card01.pib);
+        case 4:
+            printf("Comparando Número de Pontos Turísticos:\n");
+            printf("%s: %d pontos\n", card01.city, card01.total_turism_points);
+            printf("%s: %d pontos\n", card02.city, card02.total_turism_points);
 
-    printf("Qual a quantidade total de pontos turísticos da cidade?:\n");
-    scanf("%i", &card01.total_turism_points);
+            if (card01.total_turism_points > card02.total_turism_points) {
+                printf("\n%s venceu!\n", card01.city);
+            } else if (card02.total_turism_points > card01.total_turism_points) {
+                printf("\n%s venceu!\n", card02.city);
+            } else {
+                printf("\nEmpate!\n");
+            }
+            break;
 
-    printf("\nCadastre a 2° carta (2 de 2):\n");
-    _sleep(timer);
+        case 5:
+            printf("Comparando Densidade Demográfica:\n");
+            printf("%s: %.2f hab/km²\n", card01.city, density1);
+            printf("%s: %.2f hab/km²\n", card02.city, density2);
 
-    // Scan Card02 data
-    printf("Qual o estado?:\n");
-    scanf(" %c", &card02.state);
+            if (density1 < density2) {
+                printf("\n%s venceu!\n", card01.city);
+            } else if (density2 < density1) {
+                printf("\n%s venceu!\n", card02.city);
+            } else {
+                printf("\nEmpate!\n");
+            }
+            break;
 
-    printf("Digite o código da carta:\n");
-    scanf("%s", &card02.code);
+        default:
+            printf("\nOpção inválida. Tente novamente!\n");
+    }
 
-    printf("Qual o nome da cidade?:\n");
-    scanf("%s", &card02.city);
-
-    printf("Qual a população da cidade?:\n");
-    scanf("%i", &card02.population);
-
-    printf("Qual a área da cidade (em Km²)?:\n");
-    scanf("%f", &card02.area);
-
-    printf("Qual o PIB da cidade?:\n");
-    scanf("%f", &card02.pib);
-
-    printf("Qual a quantidade total de pontos turísticos da cidade?:\n");
-    scanf("%i", &card02.total_turism_points);
-
-    // Processing data
-    _sleep(timer);
-    printf("Processando informacoes...\n\n");
-
-    // Print Card01 data
-    printf("###### Carta 01 ######\n");
-    printf("Estado: %c\n", card01.state);
-    printf("Código: %s\n", card01.code);
-    printf("Cidade: %s\n", card01.city);
-    printf("População: %i\n", card01.population);
-    printf("Área: %f Km²\n", card01.area);
-    printf("PIB: R$ %f bi\n", card01.pib);
-    printf("Número de Pontos Turísticos: %i\n", card01.total_turism_points);
-
-    // Print Card02 data
-    printf("\n###### Carta 02 ######\n");
-    printf("Estado: %c\n", card02.state);
-    printf("Código: %s\n", card02.code);
-    printf("Cidade: %s\n", card02.city);
-    printf("População: %i\n", card02.population);
-    printf("Área: %f Km²\n", card02.area);
-    printf("PIB: R$ %f bi\n", card02.pib);
-    printf("Número de Pontos Turísticos: %i\n", card02.total_turism_points);
+    return 0;
 }
